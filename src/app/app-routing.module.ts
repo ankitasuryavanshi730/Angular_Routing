3f9fe75710f42f115e88b51bf0cc14d050dace2b
Ankita Suryavanshi <ankitasuryavanshi730@gmail.com>
2023-02-05 13:57:40 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"gallery",redirectTo:'/'},
+  {path:"services",component:ServicesComponent},
+  {path:"**",component:PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
